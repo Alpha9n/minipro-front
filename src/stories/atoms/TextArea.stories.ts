44db982b1950fca1b03e8813cacd6fb21f455b3f
ohs10627 <151397102+ohs10627@users.noreply.github.com>
2025-07-10 19:16:20 +0900

    add: TextAreaコンポーネントとそのスタイルを追加 (#12)
    * add: TextAreaコンポーネントとそのスタイルを追加
    
    * add: TextAreaコンポーネントとそのスタイルを追加
    
    * fix: TextAreaコンポーネントにIDプロパティを追加し、ストーリーを更新
    
    * fix: TextAreaコンポーネントのCSSからalign-selfプロパティを削除
    
    ---------
    
    Co-authored-by: alpha9n <42644369+Alpha9n@users.noreply.github.com>

---
+++ src/stories/atoms/TextArea.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { TextArea } from './TextArea';
+
+const meta = {
+  title: 'Atoms/Textarea',
+  component: TextArea,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {},
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    label: 'テキストエリア',
+    value: 'Valueサンプル',
+    id: 'textarea-example',
+  },
+};
