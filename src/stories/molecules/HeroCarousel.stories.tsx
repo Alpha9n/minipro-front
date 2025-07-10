aa37647b19cbd39837b21eda902aefbe22139566
hiroron <ohkubohiro.halstudent@gmail.com>
2025-07-10 23:26:10 +0900

    add: HeroCarouselコンポーネントとそのスタイルを追加

---
+++ src/stories/molecules/HeroCarousel.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { HeroCarousel } from './HeroCarousel';
+
+const meta: Meta<typeof HeroCarousel> = {
+  title: 'Organisms/HeroCarousel',
+  component: HeroCarousel,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof HeroCarousel>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      {
+        imageUrl: 'https://via.placeholder.com/640x360?text=完成イメージ1',
+      },
+      {
+        imageUrl: 'https://via.placeholder.com/640x360?text=完成イメージ2',
+      },
+      {
+        imageUrl: 'https://via.placeholder.com/640x360?text=完成イメージ3',
+      },
+      // {
+      //   imageUrl: 'https://via.placeholder.com/640x360?text=完成イメージ3',
+      // },
+    ],
+  },
+};
