93fc9abc9d0b3f1306170df6d78eef60788c6d24
ohs10627 <151397102+ohs10627@users.noreply.github.com>
2025-07-13 20:40:07 +0900

    update: TextAreaコンポーネントのプロパティを修正し、スタイルを調整

--- src/stories/atoms/TextArea.tsx
+++ src/stories/atoms/TextArea.tsx
@@ -1,26 +1,54 @@
 import './textArea.css';
+import React, { useEffect, useRef } from 'react';
 
 export interface TextAreaProps {
   /**ラベル */
   label: string;
   /**テキストエリアの値 */
-  value?: string;
+  value: string;
+  /**テキストエリアの値が変更されたときのイベントハンドラ */
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  /**ラベルを表示するかどうか */
+  showLabel?: boolean;
+  /**必須マークを表示するかどうか */
+  requiredMark?: boolean;
 }
 
-/** Primary UI component for user interaction */
-export const TextArea = ({ label, value }: TextAreaProps) => {
+export const TextArea = ({
+  label,
+  value,
+  onChange,
+  showLabel,
+  requiredMark = true,
+}: TextAreaProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  /**スクロール */
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.scrollTop = textareaRef.current.scrollHeight;
+    }
+  }, [value]);
+
   return (
     <div className="textarea">
-      <div className="textarea-label-group">
-        <label className="textarea-label">{label}</label>
-        <span className="textarea-required" role="alert">
-          *必須
-        </span>
-      </div>
+      {showLabel && (
+        <div className="textarea-label-group">
+          <label className="textarea-label">{label}</label>
+          {requiredMark && (
+            <span className="textarea-required" role="alert">
+              *必須
+            </span>
+          )}
+        </div>
+      )}
       <textarea
+        ref={textareaRef}
         className="textarea-input"
+        id="textarea-input"
         name="textarea-input"
         value={value}
+        onChange={onChange}
         required
       />
     </div>
