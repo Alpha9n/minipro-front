8d976cf7b27a25864a7649cf2f63f613c217d29d
hiroron <ohkubohiro.halstudent@gmail.com>
2025-07-14 18:38:17 +0900

    refactor: remove unused StoryObj import from Checkbox and Switch stories

--- src/stories/admin/atoms/Checkbox.stories.tsx
+++ src/stories/admin/atoms/Checkbox.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { Meta } from '@storybook/react-vite';
 import { Checkbox } from './Checkbox';
 import { useArgs } from 'storybook/internal/preview-api';
 
