553d9e1f408d3ce0609219318fdc4a12b1817c19
alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-07-15 12:08:54 +0900

    feat: 管理画面のルートを追加し、リダイレクトされるように変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AsgmtManagementPage } from './pages/admin/asgmt/ManagementPage';
 import { CreatePage } from './pages/admin/asgmt/CreatePage';
 import { UserManagementPage } from './pages/admin/users/UserManagementPage';
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* 管理画面 */}
+        <Route path="/admin" element={<AdminRoot />} />
         <Route path="/admin/asgmt" element={<AsgmtManagementPage />} />
         <Route path="/admin/asgmt/create" element={<CreatePage />} />
         {/* ユーザー管理 */}
@@ -16,6 +17,10 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
+}
+
+function AdminRoot() {
+  return <Navigate replace to="/admin/asgmt" />;
 }
 
 export default App;
