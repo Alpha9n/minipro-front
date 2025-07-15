6c463cc64ff399b07d3b8b1d5be673379d748f19
alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-07-15 11:42:21 +0900

    Revert "fix: Switchコンポーネントのファイル階層を移動"
    This reverts commit 66ce4d344e0c9e3dde338bdd3663c417f8f78cce.

---
+++ src/stories/atoms/Button.tsx
@@ -0,0 +1,42 @@
+import './Button.css';
+
+export interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+  size?: 'small' | 'medium' | 'large';
+  variant?: 'solid' | 'outline';
+  color?:
+    | 'green'
+    | 'red'
+    | 'orange'
+    | 'blue'
+    | 'gray'
+    | 'light'
+    | 'none'
+    | 'blue_';
+  shape?: 'default' | 'circle';
+  disabled?: boolean;
+}
+
+export const Button = ({
+  label,
+  onClick,
+  size = 'medium',
+  variant = 'solid',
+  color = 'green',
+  shape = 'default',
+}: ButtonProps) => {
+  const classes = [
+    'custom-button',
+    `btn--${size}`,
+    `btn--${variant}`,
+    `btn--${color}`,
+    shape === 'circle' ? 'btn--circle' : '',
+  ].join(' ');
+
+  return (
+    <button className={classes} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
