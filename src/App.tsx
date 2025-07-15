8fef1b6a0d6c739dabd3eafd2656a0309893a11c
alpha9n <42644369+Alpha9n@users.noreply.github.com>
2025-07-15 14:26:39 +0900

    fix: 動作させるために一旦削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { TopPage } from './pages/TopPage';
 import { EditorPage } from './pages/EditorPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/top" element={<TopPage />} />
         <Route path="/editor/:stepId" element={<EditorPage />} />
       </Routes>
     </BrowserRouter>
